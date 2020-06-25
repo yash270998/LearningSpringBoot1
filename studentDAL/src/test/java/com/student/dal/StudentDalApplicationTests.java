1c508e8a64540d78fde71ffb8a44dcf769ba11db
yash270998 <a00268810@student.ait.ie>
2020-06-25 19:35:38 +0100

    Added update delete operations in testclass

--- studentDAL/src/test/java/com/student/dal/StudentDalApplicationTests.java
+++ studentDAL/src/test/java/com/student/dal/StudentDalApplicationTests.java
@@ -1,11 +1,15 @@
 package com.student.dal;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.student.dal.entities.Student;
 import com.student.dal.repos.StudentRepository;
+
+//import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @SpringBootTest
 class StudentDalApplicationTests {
@@ -21,5 +25,35 @@ class StudentDalApplicationTests {
 		stud.setFees(30d);
 		studrepo.save(stud);
 	}
-
+	
+	@Test
+	public void testFindStudentByID() {
+		
+		Optional<Student> studOptional = studrepo.findById(1);
+		if(studOptional.isPresent()) {
+			System.out.println(studOptional.get());
+		}
+		
+	}
+	@Test
+	public void testUpdateStudent() {
+		
+		Optional<Student> studOptional = studrepo.findById(1);
+		if(studOptional.isPresent()) {
+//			System.out.println(studOptional.get());
+			Student stud = studOptional.get();
+			stud.setFees(40d);
+			studrepo.save(stud);
+		}
+		
+	}
+	
+	@Test
+	public void testDeleteStudent() {
+//		Student stud = new Student();
+//		stud.setId(1);
+//		studrepo.delete(stud);
+		studrepo.deleteById(1);
+		
+	}
 }
