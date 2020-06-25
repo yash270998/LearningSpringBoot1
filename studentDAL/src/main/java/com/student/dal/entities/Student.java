a28bb9cf1b07ce5b63421befd51bd6e7f6633566
yash270998 <a00268810@student.ait.ie>
2020-06-25 17:05:01 +0100

    Initial Commit

---
+++ studentDAL/src/main/java/com/student/dal/entities/Student.java
@@ -0,0 +1,48 @@
+package com.student.dal.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="studenttab")
+public class Student {
+	@Id
+	private int id;
+	@Column(name="sname")
+	private String name;
+	@Column(name="scourse")
+	private String course;
+	@Column(name="sfees")
+	private double fees;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public double getFees() {
+		return fees;
+	}
+	public void setFees(double fees) {
+		this.fees = fees;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", course=" + course + ", fees=" + fees + "]";
+	}
+	
+}
---
+++ studentDAL/src/main/java/com/student/dal/repos/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.student.dal.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<T, ID> {
+
+}
