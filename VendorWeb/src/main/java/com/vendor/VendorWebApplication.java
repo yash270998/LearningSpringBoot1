bcff5b17cfe4c1ae1dcc5398ad9d8075a2089a91
yash270998 <a00268810@student.ait.ie>
2020-06-28 16:35:24 +0100

    Vendor Project initialised

---
+++ VendorWeb/src/main/java/com/vendor/VendorWebApplication.java
@@ -0,0 +1,13 @@
+package com.vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorWebApplication.class, args);
+	}
+
+}
