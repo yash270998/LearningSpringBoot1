912440296520dba814b766a0c275593da7e4f199
yash270998 <a00268810@student.ait.ie>
2020-06-28 16:51:37 +0100

    edited pom.xml, serviceimpl and properties

--- VendorWeb/src/main/java/com/vendor/service/VendorServiceImpl.java
+++ VendorWeb/src/main/java/com/vendor/service/VendorServiceImpl.java
@@ -1,39 +1,53 @@
 package com.vendor.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.vendor.entities.Vendor;
-
+import com.vendor.repos.VendorRepository;
+@Service
 public class VendorServiceImpl implements VendorService {
-
+	@Autowired
+	private VendorRepository repo;
+	
 	@Override
 	public Vendor saveVendor(Vendor vendor) {
 		// TODO Auto-generated method stub
-		return null;
+		return repo.save(vendor);
 	}
 
 	@Override
 	public Vendor updateVendor(Vendor vendor) {
 		// TODO Auto-generated method stub
-		return null;
+		return repo.save(vendor);
 	}
 
 	@Override
 	public void deleteVendor(Vendor vendor) {
 		// TODO Auto-generated method stub
+		repo.delete(vendor);
 
 	}
 
 	@Override
 	public Vendor getVendorById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		Optional<Vendor> opt = repo.findById(id);
+		Vendor vendor = new Vendor();
+		if(opt.isPresent()){
+			 vendor = opt.get();
+			return vendor;
+		}
+		return vendor;
 	}
 
 	@Override
 	public List<Vendor> getAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return repo.findAll();
 	}
 
 }
