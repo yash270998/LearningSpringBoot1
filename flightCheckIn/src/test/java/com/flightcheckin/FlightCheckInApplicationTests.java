e68baa880809f3a72d769fd47870d9027937590d
yash270998 <a00268810@student.ait.ie>
2020-07-05 19:41:53 +0100

    Initial Commit Flight CheckIn

---
+++ flightCheckIn/src/test/java/com/flightcheckin/FlightCheckInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightcheckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightCheckInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
