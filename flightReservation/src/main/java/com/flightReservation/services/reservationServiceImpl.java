7304d38584d3035b3df22cfeffae67c22893b9ff
yash270998 <a00268810@student.ait.ie>
2020-07-11 13:49:51 +0100

    added Transaction management in  our system

--- flightReservation/src/main/java/com/flightReservation/services/reservationServiceImpl.java
+++ flightReservation/src/main/java/com/flightReservation/services/reservationServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.flightReservation.dto.ReservationRequest;
 import com.flightReservation.entities.Flight;
@@ -14,8 +15,9 @@ import com.flightReservation.repos.passengerRepository;
 import com.flightReservation.repos.reservationRepository;
 import com.flightReservation.util.EmailUtil;
 import com.flightReservation.util.PDFGenerator;
+
 @Service
-public class reservationServiceImpl implements reservationService{
+public class reservationServiceImpl implements reservationService {
 	Logger LOGGER = LoggerFactory.getLogger(reservationServiceImpl.class);
 	@Autowired
 	flightRepository flightrepo;
@@ -27,13 +29,15 @@ public class reservationServiceImpl implements reservationService{
 	PDFGenerator pdfgenerator;
 	@Autowired
 	EmailUtil emailutil;
+
 	@Override
+	@Transactional
 	public Reservation bookFlight(ReservationRequest request) {
 		LOGGER.info("Inside bookFlight()");
 		// TODO Auto-generated method stub
-		//Make Payment logic. not implemented here.
+		// Make Payment logic. not implemented here.
 		int flightId = request.getFlightId();
-		LOGGER.info("Flight fetched : "+flightId);
+		LOGGER.info("Flight fetched : " + flightId);
 		Flight flight = flightrepo.findById(flightId).get();
 		Passenger passenger = new Passenger();
 		passenger.setFirstName(request.getPassengerFirstName());
@@ -41,14 +45,14 @@ public class reservationServiceImpl implements reservationService{
 		passenger.setEmail(request.getPassengerEmail());
 		passenger.setPhone(request.getPassengerPhone());
 		Passenger savedPassenger = passengerrepo.save(passenger);
-		LOGGER.info("saved Passenger : "+savedPassenger);
+		LOGGER.info("saved Passenger : " + savedPassenger);
 		Reservation reservation = new Reservation();
 		reservation.setFlight(flight);
 		reservation.setPassenger(savedPassenger);
 		reservation.setCheckedIn(false);
-		
+
 		Reservation savedReservation = reservationrepo.save(reservation);
-		String pathString = "/"+savedReservation.getId()+".pdf";
+		String pathString = "/" + savedReservation.getId() + ".pdf";
 		pdfgenerator.generateItinerary(savedReservation, pathString);
 		LOGGER.info("Generating Itinerary and EMAIL");
 		emailutil.sendItinerary(passenger.getEmail(), pathString);
