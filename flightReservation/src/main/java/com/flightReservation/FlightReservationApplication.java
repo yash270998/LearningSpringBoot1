dffe43ca3ecc5914c15a3a7474f85a45dd8f3e46
yash270998 <a00268810@student.ait.ie>
2020-07-11 14:01:11 +0100

    updated proj for WAR Deployment

--- flightReservation/src/main/java/com/flightReservation/FlightReservationApplication.java
+++ flightReservation/src/main/java/com/flightReservation/FlightReservationApplication.java
@@ -2,9 +2,17 @@ package com.flightReservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlightReservationApplication {
+public class FlightReservationApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(FlightReservationApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightReservationApplication.class, args);
