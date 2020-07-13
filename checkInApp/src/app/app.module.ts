c515946a05da69222a821aabf09cb5a82066085a
yash270998 <a00268810@student.ait.ie>
2020-07-13 18:38:05 +0100

    Added 3 components and one service

--- checkInApp/src/app/app.module.ts
+++ checkInApp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StartcheckinComponent } from './components/startcheckin/startcheckin.component';
+import { CheckinComponent } from './components/checkin/checkin.component';
+import { ConfirmcheckinComponent } from './components/confirmcheckin/confirmcheckin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartcheckinComponent,
+    CheckinComponent,
+    ConfirmcheckinComponent
   ],
   imports: [
     BrowserModule
---
+++ checkInApp/src/app/components/startcheckin/startcheckin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startcheckin',
+  templateUrl: './startcheckin.component.html',
+  styleUrls: ['./startcheckin.component.css']
+})
+export class StartcheckinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
