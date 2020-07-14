06e66c88e91a159fdc64da166158f7c581897144
yash270998 <a00268810@student.ait.ie>
2020-07-14 13:56:10 +0100

    Binding reservation and the next url

--- checkInApp/src/app/components/startcheckin/startcheckin.component.ts
+++ checkInApp/src/app/components/startcheckin/startcheckin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-startcheckin',
   templateUrl: './startcheckin.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartcheckinComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router:Router) { }
+  reservationId:number
   ngOnInit(): void {
   }
-
+  onSelect(id){
+    this.router.navigate(['/checkin',id]) 
+  }
 }
