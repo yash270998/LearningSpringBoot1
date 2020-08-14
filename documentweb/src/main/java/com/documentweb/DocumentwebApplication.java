eabbf2ac300ed2df97f2f2181f6fd2220be196b6
yash270998 <yashsomani2007@gmail.com>
2020-08-14 12:32:13 +0100

    initial commit for document up/down loading on web

---
+++ documentweb/src/main/java/com/documentweb/DocumentwebApplication.java
@@ -0,0 +1,13 @@
+package com.documentweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentwebApplication.class, args);
+	}
+
+}
---
+++ documentweb/src/test/java/com/documentweb/DocumentwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
